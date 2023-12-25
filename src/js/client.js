adf42165cf49cdfcd2ffed2fad2a2ee539891f2f
bwang2 <bwang2@rocketsoftware.com>
2023-12-25 14:59:00 +0800

    Change icon

--- src/js/client.js
+++ src/js/client.js
@@ -1,8 +1,12 @@
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
-var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
+var WHITE_ICON = 'https://icanhazdadjoke.com/static/smile.svg';
+var BLACK_ICON = 'https://icanhazdadjoke.com/static/smile.svg';
 
 var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
+    console.log('Someone clicked the button');
+    return t.popup({
+        title: "Good Day!",
+        url: 'good_day.html'
+    });
 };
 
 window.TrelloPowerUp.initialize({
@@ -16,17 +20,6 @@ window.TrelloPowerUp.initialize({
       text: 'Callback',
       callback: onBtnClick,
       condition: 'edit'
-    }, {
-      // or we can also have a button that is just a simple url
-      // clicking it will open a new tab at the provided url
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'URL',
-      condition: 'always',
-      url: 'https://trello.com/inspiration',
-      target: 'Inspiring Boards' // optional target for above url
     }];
   }
 });
