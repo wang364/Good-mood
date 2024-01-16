9e48d33e0677efe76bc7c7f7590b8c8f2fff0f37
bwang2 <bwang2@rocketsoftware.com>
2024-01-16 09:38:21 +0800

    change to good mood

--- src/js/client.js
+++ src/js/client.js
@@ -4,8 +4,8 @@ var BLACK_ICON = 'https://icanhazdadjoke.com/static/smile.svg';
 var dadjokeurl = 'https://icanhazdadjoke.com/';
 var onBtnClick = function (t, opts) {
     return t.modal({
-        title: "Good Day!",
-        url: 'good_day.html'
+        title: "!",
+        url: 'good_mood.html'
     });
 };
 
