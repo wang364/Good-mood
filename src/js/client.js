fb84b977913f4cde7ef059a9d7184c4474ac0b19
bwang2 <bwang2@rocketsoftware.com>
2024-02-18 13:26:52 +0800

    1

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,3 @@
-var WHITE_ICON = 'https://icanhazdadjoke.com/static/smile.svg';
-var BLACK_ICON = 'https://icanhazdadjoke.com/static/smile.svg';
-
 var onBtnClick = function (t, opts) {
     return t.modal({
         title: "Keep a good mood!",
@@ -12,10 +9,7 @@ window.TrelloPowerUp.initialize({
   'board-buttons': function (t, opts) {
     return [{
       // we can either provide a button that has a callback function
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
+      icon: 'https://icanhazdadjoke.com/static/smile.svg',
       text: 'Good Mood',
       callback: onBtnClick,
       condition: 'edit'
